6df0f2d3aa0cc5c824156352382936fa05b9f8f3
Colten Appleby <colten.appleby@gmail.com>
2021-07-22 12:41:18 -0400

    section 8 recursion coding exercises

---
+++ coding_exercises/section8-exercises.js
@@ -0,0 +1,20 @@
+// ***************************************************
+// *** Coding Exercises from Section 8 - Recursion ***
+// ***************************************************
+
+
+// ** Coding Exercise 10: Power ** 
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(num1, num2) {
+    // should return the power of the base to the exponent
+    if (num2 === 1) {
+        return num1
+    }
+    return num1 * power(num1, num2-1)
+}
+
+console.log(power(2, 3))
+console.log(power(2, 4))
