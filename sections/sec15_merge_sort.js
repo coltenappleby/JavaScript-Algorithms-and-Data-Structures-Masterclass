9322dcb4b0d193cce904f43d1c640075f18148a0
coltenappleby <colten.appleby@gmail.com>
2021-09-01 13:03:14 -0400

    finished mergeSort

--- sections/sec15_merge_sort.js
+++ sections/sec15_merge_sort.js
@@ -11,24 +11,29 @@ const merge = (arr1, arr2) => {
         i++;
     }
     while(j < arr2.length){
-        arr.push(arr1[j])
+        arr.push(arr2[j])
         j++;
     }
     return arr
 } 
 
-clear()
-merge([5,6],[4])
-
 
 
 const mergeSort = (arr) => {
     if(arr.length <= 1) return arr;
-    let mid = arr.length/2
+    let mid = Math.floor(arr.length/2)
     let left =  mergeSort(arr.slice(0,mid));
     let right = mergeSort(arr.slice(mid));
 
     return merge(left, right)
 }
 
-merge([3, 10, 5, 4])
+clear()
+mergeSort([3, 10, 5, 4])
+mergeSort([3, 10, 5, 4, 11, 12, 13, 0, -1, 14, 5, 3, 2])
+
+
+// Big O: O(n log n)
+// 32 -> 16 -> 8 -> 4 -> 2 -> 1
+// 5 Splits -- this is log base 2
+// 2 of what power gets to 8
