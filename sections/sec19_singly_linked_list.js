9882937fead30c66a015566301f74d8a5ac6b61e
coltenappleby <colten.appleby@gmail.com>
2021-09-13 14:30:56 -0400

    push

--- sections/sec19_singly_linked_list.js
+++ sections/sec19_singly_linked_list.js
@@ -4,9 +4,32 @@ class Node {
         this.next = null
     }   
 }
+// let first = new Node(1)
+// first.next = new Node(3)
 
+class SinglyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    // push
+    push(val){ 
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 
-let first = new Node(1)
-first.next = new Node(3)
+}
 
-console.log(first.next.val)
+var list = new SinglyLinkedList()
+list.push("new value")
+list.push("new new value")
+list.push("new new new value")
