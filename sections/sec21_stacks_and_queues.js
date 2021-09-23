d9590a169eaaea8e6552f9170ff9323a3803089e
coltenappleby <colten.appleby@gmail.com>
2021-09-22 20:01:38 -0400

    node, queue, and push

---
+++ sections/sec21_stacks_and_queues.js
@@ -0,0 +1,31 @@
+// Stacks are LIFO
+// Queues are FIFO
+
+class Node{
+    constructor(value){
+        this.val = value;
+        this.prev = null;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.start = null;
+        this.end = null;
+        this.length = 0;
+    }
+
+    push(){ // add to beginning
+        let node = new Node(value)
+        if(!this.start){
+            this.start = node 
+        }
+        node.prev = this.end;
+        this.end = node ;
+        this.length++;
+    }
+
+    pop(){ // remove from beginning
+
+    }
+}
